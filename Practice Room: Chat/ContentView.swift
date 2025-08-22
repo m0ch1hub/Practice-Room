6026caa202a4516c21e6e0f077fe7773e7debc33
Aristotle F <Arijordanou@gmail.com>
2025-08-22 16:38:18 -0700

    Initial Commit

---
+++ Practice Room: Chat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Practice Room: Chat
+//
+//  Created by Aristotle Farrahi on 8/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Practice Room: Chat/Practice_Room__ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Practice_Room__ChatApp.swift
+//  Practice Room: Chat
+//
+//  Created by Aristotle Farrahi on 8/22/25.
+//
+
+import SwiftUI
+
+@main
+struct Practice_Room__ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
